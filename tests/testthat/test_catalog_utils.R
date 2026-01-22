d14671a4d7c6585dfdc1d621ead90869867cff23
Paul Linza <zplinza@gmail.com>
2026-01-22 13:34:32 -0700

    Optimized code
    Improved the expect line

--- tests/testthat/test_catalog_utils.R
+++ tests/testthat/test_catalog_utils.R
@@ -6,5 +6,5 @@ test_that("auto_streamflow_params returns a list", {
   token <- get_bearer_token()
   dataset_id <- "nws_owp_nwm_reanalysis_3_0"
   result <- auto_streamflow_params(dataset_id)
-  expect_equal(typeof(result), "list")
+  expect_type(result, "list")
 })
